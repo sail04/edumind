cafbcaad7116785991f44c92c7b655c8c42568b4
sail04 <saib909613@gmail.com>
2026-06-10 00:29:30 +0530

    chore: add vercel.json multi-service routing and configure next rewrites

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const isProd = process.env.NODE_ENV === "production";
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*",
+        destination: isProd
+          ? "/_/backend/api/:path*"
+          : "http://localhost:8000/api/:path*",
       },
     ];
   },
